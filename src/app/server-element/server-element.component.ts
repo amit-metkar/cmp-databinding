db938f9b688e0f424e7a480b3006a6e598fe71dc
Metkar <amit.metkar@corp.trizetto.com>
2017-09-20 16:19:31 +0530

    re-factored

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,20 +1,8 @@
 import {
-  Component,
-  OnInit,
-  Input,
-  ViewEncapsulation,
-  OnChanges,
-  SimpleChanges,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked,
-  OnDestroy,
-  ViewChild,
-  ElementRef,
-  ContentChild
-} from "@angular/core";
+  Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy,
+  ViewChild, ElementRef, ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -22,7 +10,8 @@ import {
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None // Emulated, Native
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, 
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   // @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name:  string;
@@ -30,45 +19,41 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   @ContentChild('contentParagraph') paragraph: ElementRef
 
   constructor() {
-    console.log('constructor called!');
-    //console.log("Header test content: "+this.header.nativeElement.textContent); // Errors out, definitely not accessible at this stage
+    console.log('constructor ---- constructor called!');
+    // console.log("Header test content: "+this.header.nativeElement.textContent); // Errors out, definitely not accessible at this stage
   }
-
   ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called!');
+    console.log('ngOnChanges ---- ngOnChanges called!');
     console.log(changes);
   }
-
   ngOnInit() {
-    console.log('ngOnInit called!');
-    console.log("Header test content: "+this.header.nativeElement.textContent); // empty, as element is not initialised at this time
-    console.log("Content paragraph: "+this.paragraph.nativeElement.textContent); //empty, as element is not initialised at this time
+    console.log('ngOnInit ---- ngOnInit called!');
+    // empty, as element is not initialised at this time
+    console.log('ngOnInit ---- Header test content: '+this.header.nativeElement.textContent); 
+    // empty, as element is not initialised at this time
+    console.log('ngOnInit ---- Content paragraph: ' +this.paragraph.nativeElement.textContent); 
   }
-
   ngDoCheck() {
-     console.log('ngDoCheck called!');
+     console.log('ngDoCheck ---- ngDoCheck called!');
   }
-
   ngAfterContentInit() {
-    console.log('ngAfterContentInit called!');
-    console.log("Header test content: "+this.header.nativeElement.textContent); // happens after ng-content initiated
-    console.log("Content paragraph: "+this.paragraph.nativeElement.textContent);
+    console.log('ngAfterContentInit ---- ngAfterContentInit called!');
+    // happens after ng-content initiated
+    console.log('ngAfterContentInit ---- Header test content: ' + this.header.nativeElement.textContent); 
+    console.log('ngAfterContentInit ---- Content paragraph: ' + this.paragraph.nativeElement.textContent);
   }
-
   ngAfterContentChecked() {
-    console.log('AfterContentChecked called!');
+    console.log('ngAfterContentChecked ---- AfterContentChecked called!');
   }
-
   ngAfterViewInit() {
-    console.log('ngAfterViewInit called!');
-    console.log("Header test content: "+this.header.nativeElement.textContent); // Here it is accessible at first time
+    console.log('ngAfterViewInit ---- ngAfterViewInit called!');
+    // Here it is accessible at first time
+    console.log('ngAfterViewInit ---- Header test content: ' + this.header.nativeElement.textContent); 
   }
-
   ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called!');
+    console.log('ngAfterViewChecked ---- ngAfterViewChecked called!');
   }
-
   ngOnDestroy() {
-    console.log('ngOnDestroy called!');
+    console.log('ngOnDestroy ---- ngOnDestroy called!');
   }
 }
